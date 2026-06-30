83e7a5c91d98c425f9342e69d13561b37a8fad9d
Yu Sun <sunyu0410@gmail.com>
2026-06-30 23:06:42 +1000

    Added padded grid to rotation sampler

--- bev/rotate.py
+++ bev/rotate.py
@@ -2,7 +2,24 @@ import SimpleITK as sitk
 import numpy as np
 
 
-def rotate_image(infile, outfile, isocentre, degree, axis="z", bg_value=-1024):
+def make_padded_grid(img, pad_voxels):
+    pad_voxels = [50, 50, 50]
+    old_size = np.array(img.GetSize())
+    old_origin = np.array(img.GetOrigin())
+    spacing = np.array(img.GetSpacing())
+
+    new_size = old_size + 2 * np.array(pad_voxels)
+    new_size = new_size.tolist()
+
+    new_origin = old_origin - (np.array(pad_voxels) * spacing)
+    new_origin = new_origin.tolist()
+
+    return new_size, new_origin
+
+
+def rotate_image(
+    infile, outfile, isocentre, degree, axis="z", bg_value=-1024, pad_voxels=None
+):
     image = sitk.ReadImage(infile)
 
     if axis == "x":
@@ -21,6 +38,14 @@ def rotate_image(infile, outfile, isocentre, degree, axis="z", bg_value=-1024):
     resampler.SetTransform(transform)
     resampler.SetInterpolator(sitk.sitkBSpline)
     resampler.SetDefaultPixelValue(bg_value)
+
+    if pad_voxels is not None:
+        new_size, new_origin = make_padded_grid(image, pad_voxels)
+        resampler.SetSize(new_size)
+        resampler.SetOutputOrigin(new_origin)
+        resampler.SetOutputSpacing(image.GetSpacing())
+        resampler.SetOutputDirection(image.GetDirection())
+
     rotated_image = resampler.Execute(image)
 
     sitk.WriteImage(rotated_image, outfile)
@@ -28,12 +53,13 @@ def rotate_image(infile, outfile, isocentre, degree, axis="z", bg_value=-1024):
 
 if __name__ == "__main__":
     rotate_image(
-        infile="data/r15.mha",
-        outfile="data/r0.mha",
+        infile="data/ct.mha",
+        outfile="data/r15p.mha",
         isocentre=[
             46.8471844842125,
             -27.777663262437926,
             -28.13538836315937,
         ],  # Physical coordinates (x, y, z)
-        degree=-15,
+        degree=15,
+        pad_voxels=[50, 50, 50],
     )
