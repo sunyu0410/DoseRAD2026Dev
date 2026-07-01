75a9e17d223dc8620bedaedf2247438ce55e3e4b
Yu Sun <sunyu0410@gmail.com>
2026-07-01 15:26:30 +1000

    Fixed the orientation of BEV

--- bev/mlc.py
+++ bev/mlc.py
@@ -19,9 +19,11 @@ def get_mlc_offsets(mlc_offsets, leaf_width=5):
 
 
     for i, offset in enumerate(mlc_offsets):
-
-        point_1 = (offset, (n_leafs-i)*leaf_width)
-        point_2 = (offset, (n_leafs-(i+1))*leaf_width)
+        # The number in MLC means moving right
+        # But the mha is LPS
+        # Hence it's -1*offset 
+        point_1 = (-offset, (i-n_leafs)*leaf_width)
+        point_2 = (-offset, ((i+1)-n_leafs)*leaf_width)
 
         pts_mm.append(point_1)
         pts_mm.append(point_2)
@@ -29,12 +31,16 @@ def get_mlc_offsets(mlc_offsets, leaf_width=5):
     # pts_mm = np.array(pts_mm)
 
     return pts_mm
+    
 
 mlc_lf = get_mlc_offsets(mlc_left)
 mlc_rt = get_mlc_offsets(mlc_right)
 
 mlc = mlc_lf + mlc_rt[::-1]
 mlc = np.array(mlc)
+mlc[:,0] += isocentre[0]
+mlc[:,1] += isocentre[2]
+
 
 print(mlc)
 
@@ -43,11 +49,12 @@ plt.axis('equal')
 plt.show()
 
 ct = sitk.ReadImage('data/ct_1mm.mha')
-mlc_idx = np.array([ct.TransformPhysicalPointToIndex((*i, isocentre[2])) for i in mlc])
+mlc_idx = np.array([ct.TransformPhysicalPointToIndex((i[0], isocentre[1], i[1])) for i in mlc])
 
-# (492, 492, 498)
-mlc_voxels = np.zeros(ct.GetSize()[::-1], dtype=np.uint8)
-rr, cc = polygon(mlc_idx[:, 0], mlc_idx[:, 1], shape=(498, 492))
+nx, ny, nz = ct.GetSize() # (498, 492, 492)
+
+rr, cc = polygon(mlc_idx[:, 0], mlc_idx[:, 2], shape=(nz, nx))
+mlc_voxels = np.zeros((nz, ny, nx), dtype=np.uint8) # (492, 492, 498)
 mlc_voxels[cc, :, rr] = 1  # Burn the filled path aperture as 1
 
 mlc_sitk = sitk.GetImageFromArray(mlc_voxels)
@@ -55,19 +62,3 @@ mlc_sitk.CopyInformation(ct)
 
 sitk.WriteImage(mlc_sitk, 'data/mlc_extrusion.mha')
 
-# #######
-
-# leaf_width = 5 # mm
-
-# left_pts_mm = []
-# n_leafs = len()
-# for i, left_offset in enumerate(mlc_left_int_mm):
-#     point_1 = (-left_offset, (40-i)*leaf_width)
-#     point_2 = (-left_offset, (40-(i+1))*leaf_width)
-
-#     left_pts_mm.append(point_1)
-#     left_pts_mm.append(point_2)
-
-# left_pts_mm = np.array(left_pts_mm)
-
-# left_pts_mm += isocentre # array([-46.84718448,  27.77766326])
